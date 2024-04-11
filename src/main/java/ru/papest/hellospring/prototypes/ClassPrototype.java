4e0f559ce051a4911661264874456a95c682bd5c
Epap <peia@inbox.ru>
2024-04-11 17:35:21 +0300

    PostConstruct method

--- src/main/java/ru/papest/hellospring/prototypes/ClassPrototype.java
+++ src/main/java/ru/papest/hellospring/prototypes/ClassPrototype.java
@@ -1,5 +1,6 @@
 package ru.papest.hellospring.prototypes;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +15,11 @@ public class ClassPrototype {
     ClassPrototype() {
         intNumber = number.incrementAndGet();
         System.out.printf("Hello, ClassPrototype %s instantiation!\n", intNumber);
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.printf("Hello, I'm ClassPrototype %s postConstruct method!\n", intNumber);
     }
 
     @Override
